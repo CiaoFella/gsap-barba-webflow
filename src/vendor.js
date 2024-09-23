9005fb05ec8f1ab5a0a10a88142e265a73b0483e
net11FellaJ <julian.fella@neteleven.de>
2024-09-23 17:39:21 +0200

    update dependencies

--- src/vendor.js
+++ src/vendor.js
@@ -3,8 +3,7 @@ import ScrollTrigger from 'gsap/ScrollTrigger.js'
 import barba from '@barba/core'
 import SplitType from 'split-type'
 import Lenis from '@studio-freight/lenis'
-import LazyLoad from 'vanilla-lazyload'
 
 gsap.registerPlugin(ScrollTrigger)
 
-export { gsap, ScrollTrigger, barba, SplitType, Lenis, LazyLoad }
+export { gsap, ScrollTrigger, barba, SplitType, Lenis }
